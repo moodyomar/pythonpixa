56089225859650476b47b5cb0444e98c657061f2
Mohammad Omar Shihab <lonak.secret@gmail.com>
2022-10-19 21:24:56 +0300

    add smoke tests

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 
 api = {
     "key": "17357236-aeda27ac71ed0d74e165db2a8",
@@ -6,9 +7,27 @@ api = {
     "q": ""
     }
 
+url = "http://localhost:3000"
+
 def apiReq(query='thailand'):
     api["q"] = query
     url = f"{api['baseurl']}/?key={api['key']}&q={api['q']}"
     data = requests.get(url)
     return data.json()['hits']
-print(apiReq("cat")[0]['webformatURL'])
+
+
+def is_url_up(url):
+    try:
+        r = requests.head(url)
+    except Exception:
+        return False
+    return r.status_code == 200
+
+
+def is_website_running(url):
+    try:
+        r = requests.get(url)
+        title = BeautifulSoup(r.text, 'html.parser')
+    except Exception:
+        return False
+    return title.find("title").get_text()
