302c1fed3bbf1c1059780a23967970cb810dd2c7
Mohammad Omar Shihab <lonak.secret@gmail.com>
2022-10-19 18:41:30 +0300

    init files structure

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask
-from markupsafe import escape
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return '<h1>Hello, World</h1>'
+def index():
+    word = request.args.get('search')
+    return render_template("index.html",searchedWord=word)
+
+@app.route('/<word>')
+def stringify(word):
+    return f'<h1>{word}? what are you trying to do?</h1>'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=3000,debug=True)
