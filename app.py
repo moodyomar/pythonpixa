bc8bf83d71db833613cf65c5d9545f4c9e297dc0
Mohammad Omar Shihab <lonak.secret@gmail.com>
2022-10-20 07:28:41 +0300

    linting + query default value

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask,render_template,request
-from utils import apiReq
+from flask import Flask, render_template, request
+from utils import apiReq, api
 
 app = Flask(__name__)
 
@@ -7,12 +7,18 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     query = request.args.get('query')
+    print(f'app query is: {query}')
+    if query is None:
+        query == api['query']
+    else:
+        api['query'] = query
+        
     data = apiReq(query)
-    return render_template("index.html",data=data,arrayLen=len(data))
+    return render_template("index.html", data=data, arrayLen=len(data))
 
 @app.route('/<word>')
 def stringify(word):
     return f'<h1>{word}? what are you trying to do?</h1>'
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=3000,debug=True)
+    app.run(host="0.0.0.0", port=3000, debug=True)
